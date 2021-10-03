29daed927d6d80c611b50a7be19c5de9c814a27b
jeweltutin <jeweltutin@gmail.com>
2021-10-03 16:56:29 +0600

    Post page comment system done using disqus

--- resources/views/website/post.blade.php
+++ resources/views/website/post.blade.php
@@ -39,11 +39,13 @@
               </p>
             </div>
             <div class="pt-5">
-              <h3 class="mb-5">6 Comments</h3>
-              <ul class="comment-list">
+            <h3 class="mb-5">6 Comments</h3>
+            <div id="disqus_thread"></div>
+              
+             <!--  <ul class="comment-list">
                 <li class="comment">
                   <div class="vcard">
-                    <!-- <img src="{{ asset('website') }}/images/person_1.jpg" alt="Image placeholder"> -->
+                     <img src="{{ asset('website') }}/images/person_1.jpg" alt="Image placeholder"> 
                     <img src="@if (!$post->user->image) {{ asset($post->user->image) }} @else {{ asset('website/images/user.png') }} @endif" alt="Image placeholder">
                   </div>
                   <div class="comment-body">
@@ -120,7 +122,7 @@
                     <p><a href="#" class="reply rounded">Reply</a></p>
                   </div>
                 </li>
-              </ul>
+              </ul> -->
               <!-- END comment-list -->
               
               <div class="comment-form-wrap pt-5">
@@ -295,4 +297,16 @@
         </div>
       </div>
     </div>
+@endsection
+
+@section('script')
+<script>
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://tutorial-16.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 @endsection
